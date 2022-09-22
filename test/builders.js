4fc96522a0019460379ecf40683c02e91ce33683
Cauê Almeida <nncl@live.com>
2022-09-21 21:33:23 -0300

    test(builders): Adds service when mocking requests

--- test/builders.js
+++ test/builders.js
@@ -5,9 +5,14 @@ import usersStub from 'test/stubs/users.json';
 import orderStub from 'test/stubs/order.json';
 import ordersStub from 'test/stubs/orders.json';
 
+import * as service from '@/database/service/orders.service';
+
+jest.mock('@/database/service');
+
 export function buildReq({ user = buildUser(), ...overrides } = {}) {
   const req = {
     user,
+    service,
     headers: { email: user.email },
     body: {},
     params: {},
