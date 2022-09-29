04d6801c8f223dbc3719dda2130bf98d5bb3cfb7
Cauê Almeida <nncl@live.com>
2022-09-28 22:28:55 -0300

    test(routes): Expects order list successfull

---
+++ src/routes/orders.router.integration.spec.js
@@ -0,0 +1,22 @@
+import supertest from 'supertest';
+import * as service from '@/database/service/orders.service';
+import app from '@/app';
+import { buildOrders, buildUser } from 'test/builders';
+
+const request = supertest(app);
+
+jest.mock('@/database/service/orders.service');
+
+describe('Router > Integration > Orders', () => {
+  it('should return status 200 and a list of orders', async done => {
+    const orders = buildOrders();
+    jest.spyOn(service, 'listOrders').mockReturnValueOnce(orders);
+
+    const res = await request.get('/api/order').set('email', buildUser().email);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({ orders });
+
+    done();
+  });
+});
